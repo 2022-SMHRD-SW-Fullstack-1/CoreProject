9085db828abf98b5d75d3c72f503b5311a4b3411
Brinicle177 <kimkr0421@naver.com>
2022-10-21 09:52:31 +0900

    라우터 생성 및 이미지파일 업로드

--- gigwork/src/App.js
+++ gigwork/src/App.js
@@ -1,9 +1,20 @@
 import './css/App.css';
+import { Routes, Route } from 'react-router-dom'
+
+import Header from './components/Header';
+import Main from './pages/Main'
+
 
 function App() {
   return (
     <div>
-      
+      <Header/>
+
+      <Routes>
+        <Route path='/' element={<Main/>}></Route>
+      </Routes>
+
+      <Footer/>
     </div>
   );
 }
