d87d5d6cb63b18d8b6060d42dd9bfa74b2d37054
Brinicle177 <kimkr0421@naver.com>
2022-10-21 17:42:09 +0900

    chat 컴포넌트 추가/설아

--- gigwork/src/App.js
+++ gigwork/src/App.js
@@ -1,7 +1,8 @@
 import './css/App.css';
 import { Routes, Route } from 'react-router-dom'
 
-import Header from './components/Header';
+import Header from './components/Header'
+import Chat from './components/Chat'
 import Footer from './components/Footer'
 import Main from './pages/Main'
 import Login from './pages/Login'
@@ -12,6 +13,7 @@ function App() {
   return (
     <div>
       <Header/>
+      <Chat/>
       <div style={{height: '60px'}}/>
       <Routes>
         <Route path='/' element={<Main/>}></Route>
--- gigwork/src/pages/Main.js
+++ gigwork/src/pages/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import help from '../asset/img/help.png'
 import solver from '../asset/img/solver.png'
 import magnif from '../asset/img/magnifGlass.png'
@@ -7,20 +7,24 @@ import '../css/Main.css'
 
 const Main = () => {
 
+    const [btnToggle, setBtnToggle] = useState(true)
+    const changeBtn = () => {
+        setBtnToggle(!btnToggle)
+    }
 
 
   return (
     <div className='top_div' id='main'>
         <div className='blankForHeader'/>
-        <div className='category'>
-            <div>
+        <div className='category' onMouseOver={() => {setBtnToggle(true)}}>
+            <div className='categoryRow'>
                 <div></div>
                 <div></div>
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
-            <div>
+            <div className='categoryRow'>
                 <div></div>
                 <div></div>
                 <div></div>
@@ -29,9 +33,9 @@ const Main = () => {
             </div>
         </div>
         <div className='buttonBox'>
-            <button className='buttonHelp'><img src={help}/><div><p>도움 요청하기</p></div></button>
-            <button className='buttonNote'><img src={note}/><div><p>의뢰글</p><p>작성하기</p></div></button>
-            <button className='buttonMagnif'><img src={magnif}/><div><p>해결사</p><p>찾아보기</p></div></button>
+            <button className='buttonHelp' id={btnToggle?'showBtn':'hideBtn'} onClick={changeBtn}><img src={help}/><div><p>도움 요청하기</p></div></button>
+            <button className='buttonNote' id={btnToggle?'hideBtn':'showBtn'}><img src={note}/><div><p>의뢰글</p><p>작성하기</p></div></button>
+            <button className='buttonMagnif' id={btnToggle?'hideBtn':'showBtn'}><img src={magnif}/><div><p>해결사</p><p>찾아보기</p></div></button>
             <button className='buttonSolver'><img src={solver}/><div><p>해결사</p><p>지원하기</p></div></button>
         </div>
         <div className='hurry'>
@@ -39,7 +43,7 @@ const Main = () => {
                 <h2>급구!</h2>
                 <span>더보기</span>
             </div>
-            <div className='realTimePost'>
+            <div className='realTimePost' onMouseOver={() => {setBtnToggle(true)}}>
                 <div></div>
                 <div>map</div>
             </div>
