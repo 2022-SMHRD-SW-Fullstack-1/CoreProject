57601447fca41f6f96df7f53b962c0fbf8a9d5cb
oh-yu-sung <vhreoddl@naver.com>
2022-10-21 11:01:49 +0900

    로그인초안/유성

--- gigwork/src/pages/Login.js
+++ gigwork/src/pages/Login.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React ,{useState}from 'react';
 
-const Login = () => {
+const Login = (props) => {
+  
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+}
+const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+}
+const onSubmitHandler = (event) => {
+    // page refresh를 막아준다
+    event.preventDefault();
+
+}
+
+
   return (
-    <div>Login</div>
+<div>
+        
+            <form >
+                <span>이메일</span>
+                <input type = "email" name='email' value = {email} onChange={onEmailHandler}/>
+                <span>비밀번호</span>
+                <input type = "password" name='password' value = {password} onChange={onPasswordHandler}/>
+                <br/>
+                <button type = 'submit' name='login'>이메일 로그인</button>
+            </form>
+        </div>
   )
 }
 
-export default Login
+export default Login;
