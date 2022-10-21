c71da60e17493f88f0106fedb84d0513303f084e
Brinicle177 <kimkr0421@naver.com>
2022-10-21 10:31:56 +0900

    import Footer /설아

--- gigwork/src/App.js
+++ gigwork/src/App.js
@@ -2,6 +2,7 @@ import './css/App.css';
 import { Routes, Route } from 'react-router-dom'
 
 import Header from './components/Header';
+import Footer from './components/Footer'
 import Main from './pages/Main'
 import Login from './pages/Login'
 import Register from './pages/Register'
