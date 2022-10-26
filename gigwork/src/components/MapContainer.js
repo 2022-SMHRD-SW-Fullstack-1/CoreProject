bb501b434a0e580ec0a3a240bb8f704ea788191d
Brinicle177 <kimkr0421@naver.com>
2022-10-26 09:41:18 +0900

    메인 색상 지정/설아

--- gigwork/src/components/MapContainer.js
+++ gigwork/src/components/MapContainer.js
@@ -6,40 +6,16 @@ const MapContainer = ({ searchPlace }) => {
 
     useEffect(() => {
         const container = document.getElementById('myMap');
-		const options = {
-			center: new kakao.maps.LatLng(35.150701, 126.870668),
-			level: 3
-		};
+        const options = {
+            center: new kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3
+        };
         const map = new kakao.maps.Map(container, options);
-
-    const ps = new kakao.maps.services.Places();
-
-    ps.keywordSearch(searchPlace, placesSearchCB);
-
-    function placesSearchCB(data, status, pagination) {
-      if (status === kakao.maps.services.Status.OK) {
-        let bounds = new kakao.maps.LatLngBounds();
-
-        for (let i = 0; i < data.length; i++) {
-          displayMarker(data[i]);
-          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-        }
-
-        map.setBounds(bounds);
-      }
-    }
-
-    function displayMarker(place) {
-      let marker = new kakao.maps.Marker({
-        map: map,
-        position: new kakao.maps.LatLng(place.y, place.x),
-      });
-    }
-  }, [searchPlace]);
+    }, []);
 
     return (
         <div id='myMap' style={{
-            width: '350px', 
+            width: '350px',
             height: '240px'
         }}></div>
     );
