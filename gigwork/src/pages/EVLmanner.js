29324bf0cbc01516b6b5ca6184f27b7885655399
SUNGJUN1234 <chotjdwns654@naver.com>
2022-11-10 13:56:21 +0900

    매너평가페이지완성/성준

--- gigwork/src/pages/EVLmanner.js
+++ gigwork/src/pages/EVLmanner.js
@@ -26,7 +26,7 @@ const [oppId,setOppId]=useState('')
 
   const [starList,setStarList] = useState([1,2,3,4,5])
 
-  const [point,setPoint] =useState(5);
+  const [point,setPoint] =useState('star4.png');
 
 const changeStar=(e)=>{
 
@@ -37,11 +37,6 @@ const changeStar=(e)=>{
         document.querySelectorAll('img')[7].setAttribute('src',star)
         document.querySelectorAll('img')[8].setAttribute('src',star)
         setPoint('star5.png')
-        axios
-        .post('/gigwork/profile/evl5',oppNick)
-        .then(res=>console.log(res))
-        .catch(e=>console.log(e))
-
     }else if(e.target.id==4){
         document.querySelectorAll('img')[4].setAttribute('src',star)
         document.querySelectorAll('img')[5].setAttribute('src',star)
@@ -49,10 +44,6 @@ const changeStar=(e)=>{
         document.querySelectorAll('img')[7].setAttribute('src',star)
         document.querySelectorAll('img')[8].setAttribute('src',bStar)
         setPoint('star4.png')
-        axios
-        .post('/gigwork/profile/evl4',oppNick)
-        .then(res=>console.log(res))
-        .catch(e=>console.log(e))
     }else if(e.target.id==3){
         document.querySelectorAll('img')[4].setAttribute('src',star)
         document.querySelectorAll('img')[5].setAttribute('src',star)
@@ -67,10 +58,6 @@ const changeStar=(e)=>{
         document.querySelectorAll('img')[7].setAttribute('src',bStar)
         document.querySelectorAll('img')[8].setAttribute('src',bStar)
         setPoint('star2.png')
-        axios
-        .post('/gigwork/profile/evl2',oppNick)
-        .then(res=>console.log(res))
-        .catch(e=>console.log(e))
     }else{
         document.querySelectorAll('img')[4].setAttribute('src',star)
         document.querySelectorAll('img')[5].setAttribute('src',bStar)
@@ -78,10 +65,6 @@ const changeStar=(e)=>{
         document.querySelectorAll('img')[7].setAttribute('src',bStar)
         document.querySelectorAll('img')[8].setAttribute('src',bStar)
         setPoint('star1.png')
-        axios
-        .post('/gigwork/profile/evl1',oppNick)
-        .then(res=>console.log(res))
-        .catch(e=>console.log(e))
     }
 
 }
@@ -107,11 +90,32 @@ useEffect(()=>{
     .post('/gigwork/profile/nameToId',oppNick)
     .then(res=>setOppId(res.data))
     .catch(e=>console.log(e))
+    if(point == 'star5.png'){
+        axios
+        .post('/gigwork/profile/evl5',oppNick)
+        .then(res=>console.log(res))
+        .catch(e=>console.log(e))
+    }else if(point == 'star4.png'){
+        axios
+        .post('/gigwork/profile/evl4',oppNick)
+        .then(res=>console.log(res))
+        .catch(e=>console.log(e))
+    }else if(point == 'star2.png'){
+        axios
+        .post('/gigwork/profile/evl2',oppNick)
+        .then(res=>console.log(res))
+        .catch(e=>console.log(e))
+    }else if(point == 'star1.png'){
+        axios
+        .post('/gigwork/profile/evl1',oppNick)
+        .then(res=>console.log(res))
+        .catch(e=>console.log(e))
+    }
 },[oppNick,sayInfo])
 
 useEffect(()=>{
     setEvlInfo({targetId:oppId,oppId:myId,evlPoint:point,evlContent:sayInfo,evlDate:today})
-},[oppId,myId,point,sayInfo])
+},[oppId,myId,point,sayInfo,setPoint])
 
 const saveEvl=()=>{
     axios
