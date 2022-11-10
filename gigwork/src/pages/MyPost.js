10d7a579da8ccca22ba235d19552fbb0d44e3cc0
SUNGJUN1234 <chotjdwns654@naver.com>
2022-11-10 18:03:34 +0900

    내 게시물 확인 페이지 완성/성준

--- gigwork/src/pages/MyPost.js
+++ gigwork/src/pages/MyPost.js
@@ -1,39 +1,62 @@
-import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import '../css/MyBookmark.css'
 import MPmenu from '../components/MPmenu'
+import { useNavigate } from 'react-router-dom'
+
 
 const MyPost = () => {
+    var myId = { mem_id: (localStorage.id) }
+  
+    const [postList,setPostList]=useState([{}])
+    useEffect(() => {
 
-    const [receiveList, setReceiveList] = useState([])
-
-    useEffect(()=>{
         axios
-        .post('gigwork/post/postlist')
-        .then((res)=>{
-            setReceiveList(res.data)
-            console.log(res.data)
-            .catch(error=>console.log(error))
-        })
-    }, [])
-
-
-  return (
+          .post('gigwork/profile/bringPost', myId)
+          .then(res => {setPostList( res.data.JasonArray)
+                    console.log(res.data.JasonArray)})
+          .catch(e => console.log('에러!', e))
+      }, [])
+  
+  const resPostList = postList.map((item,idx)=>
+      <tr key={item+idx}>
+        <td>{item.post_num}</td>
+        <td><img src={item.urgent}></img></td>
+        <td>{item.title}</td>
+        <td><img src={item.post_offer_yn} className='jeImg'></img></td>
+        <td>{item.post_pay} 원</td>
+      </tr>
+  )
+  
+  
+    return (
     <div className='top_div'>
-        <MPmenu></MPmenu>
-        <div>
-            <span>찜목록</span>
-            <div>
-                <button>찜취소</button>
-                <input type='checkbox'/> 
-                <span>스크랩일</span>
-                <span>제목</span>
-                <span>근무일자</span>
-                <span>근무장소</span>
-                <span>수당</span>
-            </div>
+      <MPmenu />
+
+      <div className='wholeBox'>
+
+
+        <div className='bookmarkHeader'>
+          <button id='cancelbtn'>글 정렬</button>
         </div>
+        <div className='contentpart'>
+          <div className='oneContent'>
+           
+            
+            </div>
+            <table id='tablepart'>
+                <th><span>게시글 번호</span></th>
+                <th><span>급구</span></th>
+                <th><span>게시글 제목</span></th>
+                <th><span>제의받기</span></th>
+                <th><span>수당</span></th>
+                {resPostList}
+            </table>
 
 
+         
+        </div>
+      </div >
 
     </div>
   )
